08d65423dd191fb7553ce7cff9ad79be5cd9b660
labliuk <lucas.labliuk@hotmail.com>
2017-08-27 22:44:39 -0300

    Add files via upload

---
+++ Exercicio1.py
@@ -0,0 +1,19 @@
+lista = []
+
+for i in range(0,10):
+        a = int(input("Digite um numero: "))
+        lista.append(a)
+
+def troca(lista):
+    c = 0
+    for i in lista:
+        if i > 0:
+            lista[c] = 1
+        else:
+            lista[c] = 0
+        c = c+1
+    return lista
+
+lista = troca(lista)
+
+print (lista)
